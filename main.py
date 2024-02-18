5ef64b128d2e88d0d6adfc5bb352afbc43514c06
Mathis Verstrepen <mathisverstrepen@gmail.com>
2024-02-18 15:41:24 +0100

    🚑 Fix file creation on first launch

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ from dotenv import load_dotenv
 from src.ugc_posters import get_all_posters
 from src.discord_bot import DiscordBot
 
-# Creates empty JSON file to compare to if this is the first check #
-if not pathlib.Path(".previous_posters.json").is_file():
-    with open(".previous_posters.json", "w+") as f_out:
-        json.dump({}, f_out)
-
 # Loading environment variables #
 if pathlib.Path("/.dockerenv").exists():
     print("Running in Docker")
@@ -23,6 +18,11 @@ else:
 
 if __name__ == "__main__":
     discord_bot = DiscordBot()
+    
+    # Creates empty JSON file to compare to if this is the first check #
+    if not pathlib.Path(".previous_posters.json").is_file():
+        with open(".previous_posters.json", "w+") as f_out:
+            json.dump({}, f_out)
     
     # Loading previous data for future comparison #
     with open(".previous_posters.json", "r") as f_in:
